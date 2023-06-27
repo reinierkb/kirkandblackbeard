2ff7bf6da0b6454cbb672fdbdc20d1f2f18ed4a4
Reinier Ladan <reinierl@gmail.com>
2023-06-27 13:04:13 +0000

    Improved codespaces support

--- src/_data/kbsite.js
+++ src/_data/kbsite.js
@@ -1,6 +1,9 @@
 module.exports = function() {
-  if (process.env.DEVCONTAINER) {
-      baseurl = "/kbsite";
+
+  const env = process.env;
+
+  if (env.CODESPACES) {
+      baseurl = "https://" + env.CODESPACE_NAME + "-8080." + env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN + "/" + env.RepositoryName;
   } else {
       baseurl = "";
   }
